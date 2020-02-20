83168095107775340f968af46df02d7d8a02abff
Larry Mckuydee <larry.mckuydee@gmail.com>
2020-02-20 09:53:30 +0000

    Update adminarchitect to use latest change of asset path so that no clash with admin route and cause 403 Change class Users resource to Employees for core user module for adminarchitect

--- app/Http/Terranet/Administrator/Modules/Users.php
+++ app/Http/Terranet/Administrator/Modules/Users.php
@@ -2,11 +2,34 @@
 
 namespace App\Http\Terranet\Administrator\Modules;
 
-use Terranet\Administrator\Modules\Users as CoreUsersModule;
+use App\User;
+use Terranet\Administrator\Contracts\Module\Editable;
+use Terranet\Administrator\Contracts\Module\Exportable;
+use Terranet\Administrator\Contracts\Module\Filtrable;
+use Terranet\Administrator\Contracts\Module\Navigable;
+use Terranet\Administrator\Contracts\Module\Sortable;
+use Terranet\Administrator\Contracts\Module\Validable;
+use Terranet\Administrator\Scaffolding;
+use Terranet\Administrator\Traits\Module\AllowFormats;
+use Terranet\Administrator\Traits\Module\AllowsNavigation;
+use Terranet\Administrator\Traits\Module\HasFilters;
+use Terranet\Administrator\Traits\Module\HasForm;
+use Terranet\Administrator\Traits\Module\HasSortable;
+use Terranet\Administrator\Traits\Module\ValidatesForm;
 
 /**
- * Administrator Users Module.
+ * Administrator Resource Users
+ *
+ * @package Terranet\Administrator
  */
-class Users extends CoreUsersModule
+class Users extends Scaffolding implements Navigable, Filtrable, Editable, Validable, Sortable, Exportable
 {
+    use HasFilters, HasForm, HasSortable, ValidatesForm, AllowFormats, AllowsNavigation;
+
+    /**
+     * The module Eloquent model
+     *
+     * @var string
+     */
+    protected $model = User::class;
 }
